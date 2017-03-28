02ce53dc2d43cf80deb7847fff7510ed8b8f702c
Mindaugas Dabulskis <mida@danskebank.dk>
2017-03-28 13:33:40 +0300

    Some changes

--- SayToPay.REST/Contract/PayToSayResponse.cs
+++ SayToPay.REST/Contract/PayToSayResponse.cs
@@ -6,12 +6,6 @@
 
         public string DisplayText { get; set; }
 
-        public string Data { get; set; }
-
-        public string ContextOut { get; set; }
-
         public string Source { get; set; }
-
-        public string FollowupEvent { get; set; }
     }
 }
